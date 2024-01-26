262bd0bac7da6d0e22a7844462610050353e374d
beatsey <lixa4ik1@gmail.com>
2024-01-26 21:57:35 +0300

    new tasks

---
+++ phone_numbers.cpp
@@ -0,0 +1,55 @@
+﻿#include <vector>
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+char selectLetter(char l, set<char>& letters) {
+    bool b = false;
+    for (auto& c : letters) {
+        if (b) {
+            return c;
+        }
+        if (c == l) {
+            b = true;
+        }
+        
+    }
+    return ' ';
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    set<char> letters;
+    for (char l: s) {
+        letters.insert(l);
+    }
+    if (k > n) {
+        s.resize(k);
+        for (int i = n; i < k; i++) {
+            s[i] = *(letters.begin());
+        }
+        cout << s;
+        return 0;
+    }
+    string t;
+    int i;
+    for (i = k - 1; i >= 0; i--) {
+        if (s[i] != *(--letters.end())) {
+            t = s.substr(0, i);
+            t.resize(k);
+            t[i] = selectLetter(s[i], letters);
+            i++;
+            break;
+        }
+    }
+    for (; i < k; i++) {
+        t[i] = *(letters.begin());
+    }
+    cout << t;
+}
---
+++ points_on_line.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, d;
+    cin >> n >> d;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    sort(nums.begin(), nums.end());
+    int  i = 0, j = 0;
+    int mx = 1;
+    for (; i < nums.size(); i++) {
+        if (nums[i] - nums[j] <= d) {
+            mx = max(mx, i - j + 1);
+        }
+        for (; nums[i] - nums[j] > d; j++);
+    }
+    cout << nums.size() - mx;
+}
